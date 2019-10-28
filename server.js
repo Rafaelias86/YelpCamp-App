dffa70852c2afa8661d93346b573e46ab39a7fae
Rafa Elias <rafaeliasuzcategui@gmail.com>
2019-10-28 18:03:35 -0400

    Views, partials and routes added

--- server.js
+++ server.js
@@ -1,43 +1,91 @@
 var express = require("express");
 var app = express();
+var PORT = process.env.PORT || 7000;
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+require("dotenv").config();
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/yelp_camp";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Granite Hill", image: "https://www.photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1464d53a514f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Salmon Creek", image: "https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Granite Hill", image: "https://www.photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1464d53a514f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Salmon Creek", image: "https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Granite Hill", image: "https://www.photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1464d53a514f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"},
-    {name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722d7bd7904bc55c_960.jpg&user=Pexels"}
-];
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
 
-app.get("/", function(req, res){
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Laguna El Suero", 
+//         image: "https://s1.wklcdn.com/image_67/2011857/35139636/22937242Master.jpg",
+//         description: "This is a huge Granite Hill no water no bathrooms. Beautiful Granite"
+//     }, function (err, campground) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Newly Created Campground: ");
+//             console.log(campground);
+//         }
+//     });
+
+
+app.get("/", function (req, res) {
     res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+app.get("/campgrounds", function (req, res) {
+    //Get all the campgrounds from DB
+    Campground.find({}, function(err, campgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index", { campgrounds: campgrounds });
+        }
+    });
 });
 
-app.post("/campgrounds", function(req, res){
+app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
-    var image =req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = { name: name, image: image, description: desc};
+    //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            //redirect to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    })
 });
 
-app.get("/campgrounds/new", function(req, res){
+app.get("/campgrounds/new", function (req, res) {
     res.render("new")
 });
 
-app.listen(3000 , function(){
-    console.log("Listen PORT 3000")
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provieded ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+             //render show template with that campground
+             res.render("show", {campground: foundCampground});
+        }
+    });
 });
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
+
+
